76c2cbae1baff00c2bb8005ad455b9dda6d1d406
ltonetti <ltonett1@jhu.edu>
2025-05-15 14:46:01 -0400

    Add print statement to test commit

---
+++ news_article_analysis.py
@@ -0,0 +1,10 @@
+import sys
+print(sys.executable)
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Starting analysis
+print("Ready to analyze!")
+print("This is a test commit")
